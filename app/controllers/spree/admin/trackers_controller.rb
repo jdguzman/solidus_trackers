05cfa60372bbe4c80a6b650b1472053e2c41c5e8
Julianna Tetreault <juliannatetreault@gmail.com>
2019-08-08 11:01:23 -0600

    Fixed rubocop errors [ch6306]

--- app/controllers/spree/admin/trackers_controller.rb
+++ app/controllers/spree/admin/trackers_controller.rb
@@ -1,5 +1,4 @@
 class Spree::Admin::TrackersController < Spree::Admin::ResourceController
-
     before_action :find_tracker, only: [:edit, :update, :destroy]
 
     def index
@@ -17,43 +16,42 @@ class Spree::Admin::TrackersController < Spree::Admin::ResourceController
     def create
       @tracker = Spree::Tracker.new(tracker_params)
 
-        if @tracker.respond_to?(:store_id=)
-          @tracker[:store_id] = current_store&.id
-        end
-
-        if @tracker.save
-          flash[:success] = "Tracker created"
-          redirect_to admin_trackers_path
-        else
-          render :new
-        end
+      if @tracker.respond_to?(:store_id=)
+        @tracker[:store_id] = current_store.id
       end
 
-      def edit
+      if @tracker.save
+        flash[:success] = "Tracker created"
+        redirect_to admin_trackers_path
+      else
+        render :new
       end
+    end
 
-      def update
-        if @tracker.update_attributes(tracker_params)
-          flash[:success] = "Tracker updated"
-          redirect_to admin_trackers_path
-        else
-          render :edit
-        end
+    def edit
+    end
+
+    def update
+      if @tracker.update_attributes(tracker_params)
+        flash[:success] = "Tracker updated"
+        redirect_to admin_trackers_path
+      else
+        render :edit
       end
+    end
 
-      def destroy
-        @tracker.destroy
+    def destroy
+      @tracker.destroy
 
-        flash[:success] = "Tracker '#{@tracker.name}' deleted"
+      flash[:success] = "Tracker '#{@tracker.name}' deleted"
 
-        respond_with(@tracker) do |format|
-          format.html { redirect_to admin_trackers_path }
-          format.js { render_js_for_destroy }
-        end
+      respond_with(@tracker) do |format|
+        format.html { redirect_to admin_trackers_path }
+        format.js { render_js_for_destroy }
       end
+    end
 
-
-    private
+  private
 
     def tracker_params
       params.require(:tracker).permit(:analytics_id, :active, :store_id, :tracker_type, :name, :script, :options, :app_id)
@@ -62,5 +60,4 @@ class Spree::Admin::TrackersController < Spree::Admin::ResourceController
     def find_tracker
       @tracker = Spree::Tracker.where(store_id: current_store.id).find(params[:id])
     end
-
 end
